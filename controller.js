3f6cb906362b99ee25e7d1c73fbcd255024f7a11
shokri1371 <shokri.zahra@znu.ac.ir>
2021-02-22 14:08:58 +0330

    added storage_todo to the repo

---
+++ controller.js
@@ -0,0 +1,43 @@
+class Controller {
+    constructor(model, view) {
+      this.model = model
+      this.view = view
+
+      // Explicit this binding
+      this.model.bindTodoListChanged(this.onTodoListChanged)
+      this.view.bindAddTodo(this.handleAddTodo)
+      this.view.bindEditTodo(this.handleEditTodo)
+      this.view.bindDeleteTodo(this.handleDeleteTodo)
+      this.view.bindToggleTodo(this.handleToggleTodo)
+      this.view.bindFilterTodo(this.handleFilterTodo);
+      // Display initial todos
+      this.onTodoListChanged(this.model.todos)
+    }
+      
+    onTodoListChanged = todos => {
+      this.view.displayTodos(todos)
+    }
+  
+    handleAddTodo = todoText => {
+      this.model.addTodo(todoText)
+    }
+  
+    handleEditTodo = (id, todoText) => {
+      this.model.editTodo(id, todoText)
+    }
+  
+    handleDeleteTodo = id => {
+      this.model.deleteTodo(id)
+    }
+  
+    handleToggleTodo = id => {
+      this.model.toggleTodo(id)
+    }
+    handleFilterTodo = filter => {
+      this.model.filterTodo(filter);
+  };
+  
+  }
+  
+  const app = new Controller(new Model(), new View())
+  
